58dffa3f874e0f5e59c739874341b6a8cd11db90
Shaku-Med <jujubelt124@gmail.com>
2024-12-21 01:47:15 -0500

    s

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import Broadcaster from './components/Broadcaster'
 import Viewer from './components/Viewer'
-
+import React from 'react'
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-center p-24">
